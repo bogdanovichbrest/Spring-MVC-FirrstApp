de5cd3b6bad1d0edcc906c8b72325bed2b5eb8d6
RostreiD <RostreiD@computer-b4d4ca>
2017-02-27 13:00:06 +0200

    Edit method implemented

--- spring-mvc-firstApp/src/main/java/jp/hanazono/webmvc/controller/AppController.java
+++ spring-mvc-firstApp/src/main/java/jp/hanazono/webmvc/controller/AppController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.hanazono.webmvc.dao.PersonDAO;
+import jp.hanazono.webmvc.domain.Person;
 
 @Controller
 @RequestMapping("/")
@@ -20,24 +21,33 @@ public class AppController {
 	private PersonDAO personDAO;
 
 	@RequestMapping("/")
-	public String redirectIndex(ModelMap model)
-	{
-		model.addAttribute("author","Alexander Bogdanovich");
+	public String redirectIndex(ModelMap model) {
+		model.addAttribute("author", "Alexander Bogdanovich");
 		model.addAttribute("Persons", personDAO.getAllPersons());
 		return "index";
 	}
-	
+
 	@RequestMapping("/delete")
-	public String deletePerson(HttpServletRequest request)
-	{
+	public String deletePerson(HttpServletRequest request) {
 		Integer id = Integer.parseInt(request.getParameter("id"));
 		personDAO.deletePerson(personDAO.findByID(id));
 		return "redirect:/";
 	}
-	
+
 	@RequestMapping("/edit")
-	String editPerson(HttpServletRequest request)
-	{
+	String editPerson(HttpServletRequest request, ModelMap model) {
+		if (request.getParameter("id") != null) {
+			Person person = new Person();
+			person = personDAO.findByID(Integer.parseInt(request.getParameter("id")));
+			model.addAttribute("person", person);
+		}
+
 		return "edit";
+	}
+
+	@RequestMapping("/save")
+	public String savePerson() {
+		return "redirect:/";
+
 	}
 }
